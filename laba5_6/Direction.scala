85b9b16e4abf656a1f0f6f4909300a0edfd86dd4
KseniiaHondak <32408073+KseniiaHondak@users.noreply.github.com>
2019-12-19 20:53:19 +0200

    Add files via upload
    add files for laba5_6

---
+++ laba5_6/Direction.scala
@@ -0,0 +1,8 @@
+package ua.nure.msit.laba5_6
+
+trait Direction
+
+case object Up extends Direction
+case object Down extends Direction
+case object Left extends Direction
+case object Right extends Direction
---
+++ laba5_6/WumpusPercept.scala
@@ -0,0 +1,7 @@
+package ua.nure.msit.laba5_6
+
+case class WumpusPercept(glitter: Boolean,
+                         stench: Boolean,
+                         breeze: Boolean,
+                         bump: Boolean,
+                         scream: Boolean)
