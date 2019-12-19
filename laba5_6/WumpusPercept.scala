84fdd694a2421739228a0ebe441c6b4dd4893b8b
KseniiaHondak <32408073+KseniiaHondak@users.noreply.github.com>
2019-12-19 22:57:40 +0300

    Revert "Add files via upload"
    This reverts commit 85b9b16e4abf656a1f0f6f4909300a0edfd86dd4.

--- laba5_6/WumpusPercept.scala
+++
@@ -1,7 +0,0 @@
-package ua.nure.msit.laba5_6
-
-case class WumpusPercept(glitter: Boolean,
-                         stench: Boolean,
-                         breeze: Boolean,
-                         bump: Boolean,
-                         scream: Boolean)
